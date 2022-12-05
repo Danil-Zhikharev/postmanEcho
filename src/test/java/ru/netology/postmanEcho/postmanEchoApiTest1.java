49550fcc40909c31d5d7b043fa43171ad268f00a
Danil Zhikharev <dane99@mail.ru>
2022-12-05 22:01:48 +0500

    Commit_22:01_05.12

--- src/test/java/ru/netology/postmanEcho/postmanEchoApiTest1.java
+++ src/test/java/ru/netology/postmanEcho/postmanEchoApiTest1.java
@@ -1,23 +1,39 @@
 package ru.netology.postmanEcho;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class postmanEchoApiTest1 {
+    RequestSpecification requestSpecification;
+    Response response;
+    ValidatableResponse validatableResponse;
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://postman-echo.com";
+    }
+
     @Test
-    void shouldReturnNothing() {
-        // Given - When - Then
-        // Предусловия
-        given()
-                .baseUri("https://postman-echo.com/post?hand=wave")
-                .body("lol")
-                // Выполняемые действия
-                .when()
-                .post("/post")
-                // Проверки
-                .then()
-                .statusCode(200)
-                .body("body", equalTo("lol"));
+    void postManifestN1() {
+        String requestBody = "{\"name\":\"myFirstPostApiByJava\",\"date\":\"24.02.2022\",\"message\":\"noWar\"}";
+        requestSpecification = given();
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.body(requestBody);
+        response = requestSpecification.post("/post");
+        String responseString = response.prettyPrint();
+        validatableResponse = response.then();
+        validatableResponse.statusCode(200);
+        validatableResponse.statusLine("HTTP/1.1 200 OK");
+        validatableResponse.body("data.name", equalTo("myFirstPostApiByJava"));
+        validatableResponse.body("data.date", equalTo("24.02.2022"));
+        validatableResponse.body("data.message", equalTo("noWar"));
     }
 }
